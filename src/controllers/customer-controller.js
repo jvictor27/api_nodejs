5188decf945595ec7bab25b01a606dde1ebe9819
jvictor27 <jvictor451@gmail.com>
2018-10-29 19:19:44 -0300

    encriptando senha (md5+SALT)

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -2,6 +2,7 @@
 
 const ValidationContract = require('../validators/fluent-validator');
 const repositoryCustomer = require('../repositories/customer-repository');
+const md5 = require('md5');
 
 exports.post = async(req, res, next) => {
     let contract = new ValidationContract();
@@ -16,8 +17,11 @@ exports.post = async(req, res, next) => {
     }
 
     try {
-        await repositoryCustomer
-            .create(req.body);
+        await repositoryCustomer.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: md5(req.body.password + global.SALT_KEY)
+        });
         res.status(201).send({message: 'Cliente cadastrado com sucesso!'});
     } catch(e) {
         res.status(400).send({
