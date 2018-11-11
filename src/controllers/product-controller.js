d6df44f1fb26fee019c84d458a124432c9a3f3cb
jvictor27 <jvictor451@gmail.com>
2018-11-11 12:58:53 -0300

    Revert "Configurando Aamazon AWS S3 para fazer upload e assim armazenar a imagem do produto."
    This reverts commit feb5a39e09a2e42d7a831213647f4e6c63c980a0.

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,20 +1,7 @@
 'use-strict';
 
-const validationContract = require('../validators/fluent-validator');
+const ValidationContract = require('../validators/fluent-validator');
 const repositoryProduct = require('../repositories/product-repository');
-const config = require('../config');
-const AWS = require('aws-sdk');
-const fs = require('fs');
-const path = require('path');
-const mime = require('mime');
-
-// Configurando o AWS
-AWS.config.update({
-    accessKeyId: config.awsAccessKeyId,
-    secretAccessKey: config.awsSecretAccessKey
-});
-
-var s3 = new AWS.S3();
 
 exports.get = async(req, res, next) => {
     try {
@@ -67,7 +54,7 @@ exports.getByTag = async(req, res, next) => {
 }
 
 exports.post = async(req, res, next) => {
-    let contract = new validationContract();
+    let contract = new ValidationContract();
     contract.hasMinLen(req.body.title, 3, 'O título deve conter ao menos 3 caracteres.');
     contract.hasMinLen(req.body.slug, 3, 'O slug deve conter ao menos 3 caracteres.');
     contract.hasMinLen(req.body.description, 3, 'A descrição deve conter ao menos 3 caracteres.');
@@ -79,28 +66,8 @@ exports.post = async(req, res, next) => {
     }
 
     try {
-        let mimeType = mime.getType(req.body.image);
-
-        let paramsS3 = {
-            Bucket: config.bucketImageName,
-            Body: fs.createReadStream(req.body.image),
-            Key: "products/"+Date.now()+"_"+path.basename(req.body.image),
-            ContentType: mimeType, 
-            ACL:  'public-read' 
-        }
-
-        let putObjectPromise = await s3.upload(paramsS3).promise();
-        let imageLocation = putObjectPromise.Location;
-
-        await repositoryProduct.create({
-            title: req.body.title,
-            slug: req.body.slug,  
-            description: req.body.description,
-            price: req.body.price,   
-            active: true,
-            tags: req.body.tags,
-            image: imageLocation 
-        });
+        await repositoryProduct
+            .create(req.body);
         res.status(201).send({message: 'Produto cadastrado com sucesso!'});
     } catch(e) {
         res.status(400).send({
